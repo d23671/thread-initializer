fc9ce58f9d4812c3ba56a4629e54c5493f03507b
Putta Khunchalee <thaikore@hotmail.com>
2013-06-22 09:16:52 +0700

    Initial Project.

---
+++ ThreadInitializer/stdafx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Compilation Controlling Header Files:
+
+#include "targetver.h"
+
+// Windows Header Files:
+
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+
+#include <windows.h>
