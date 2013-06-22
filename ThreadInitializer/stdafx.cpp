33820fc66cbcbb676aaa364b775329862af44213
Putta Khunchalee <thaikore@hotmail.com>
2013-06-22 11:33:25 +0700

    Add handlers container object.

--- ThreadInitializer/stdafx.cpp
+++ ThreadInitializer/stdafx.cpp
@@ -1,1 +1,4 @@
 #include "stdafx.h"
+
+SRWLOCK g_srwHandlers = SRWLOCK_INIT;
+std::list<HANDLERSINFO> g_handlers;
--- ThreadInitializer/stdafx.h
+++ ThreadInitializer/stdafx.h
@@ -9,3 +9,25 @@
 #define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
 
 #include <windows.h>
+
+// C++ Header Files:
+
+#include <list>
+
+// Project Header Files:
+
+#include "ThreadInitializer.h"
+
+// Types Definition:
+
+struct HANDLERSINFO
+{
+	LPTHREAD_INITIALIZER_ROUTINE pfnInitializer;
+	LPTHREAD_INITIALIZER_CLEANUP pfnCleanup;
+	LPVOID lpContext;
+};
+
+// Global Variables:
+
+extern SRWLOCK g_srwHandlers;
+extern std::list<HANDLERSINFO> g_handlers;
