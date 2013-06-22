d7b953cc79e244688dad77c441c112acfcf7ce0a
Putta Khunchalee <thaikore@hotmail.com>
2013-06-22 11:42:09 +0700

    Finish AddThreadInitializerEx function.

--- ThreadInitializer/ThreadInitializer.cpp
+++ ThreadInitializer/ThreadInitializer.cpp
@@ -11,11 +11,23 @@ BOOL WINAPI AddThreadInitializer(LPTHREAD_INITIALIZER_ROUTINE InitializerRoutine
 
 BOOL WINAPI AddThreadInitializerEx(LPTHREAD_INITIALIZER_ROUTINE InitializerRoutine, LPTHREAD_INITIALIZER_CLEANUP CleanupRoutine, LPVOID Context)
 {
+	HANDLERSINFO handlersInfo;
+
 	if (InitializerRoutine == nullptr)
 	{
 		SetLastError(ERROR_INVALID_PARAMETER);
 		return FALSE;
 	}
+
+	ZeroMemory(&handlersInfo, sizeof(handlersInfo));
+
+	handlersInfo.pfnInitializer = InitializerRoutine;
+	handlersInfo.pfnCleanup = CleanupRoutine;
+	handlersInfo.lpContext = Context;
+
+	AcquireSRWLockExclusive(&g_srwHandlers);
+	g_handlers.push_back(handlersInfo);
+	ReleaseSRWLockExclusive(&g_srwHandlers);
 
 	return TRUE;
 }
